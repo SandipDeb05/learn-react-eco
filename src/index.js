f2ba6d70282dcca50a502d2de6596a460f77fea1
SandipDeb <sandipdeb05@gmail.com>
2023-08-30 17:59:34 +0530

    leanded basic testing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import ContextView from "./context/ContextView";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ContextView />
+    <App />
   </React.StrictMode>
 );
 
