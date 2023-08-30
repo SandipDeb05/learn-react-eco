6559a14a241b7794ae0dc7e0d12924ab032f4475
SandipDeb <sandipdeb05@gmail.com>
2023-08-30 22:06:21 +0530

    leanded getBy_ queries in testing

--- src/components/Application/Application.js
+++ src/components/Application/Application.js
@@ -1,32 +1,54 @@
 const Application = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
-      </div>
+    <>
+      <h1>Job application form</h1>
+      <h2>Section 1</h2>
+      <p>ALl field are mandatory</p>
+      <span title="close">X</span>
 
-      <div>
-        <label htmlFor="job-location">Job location</label>
-        <select id="job-location">
-          <option value="">Select a country</option>
-          <option value="US">United States</option>
-          <option value="GB">United Kingdom</option>
-          <option value="CA">Canada</option>
-          <option value="IN">India</option>
-          <option value="AU">Australia</option>
-        </select>
-      </div>
+      <img src="https://dummyimage.com/200x100" alt="a person with a laptop" />
 
-      <div>
-        <label>
-          <input type="checkbox" id="terms" /> I agree to the terms and
-          condition
-        </label>
-      </div>
+      <div data-testid="custom-element">Custom HTML Element</div>
 
-      <button>Submit</button>
-    </form>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Fullname"
+            value="Sandip Deb"
+            onChange={() => {}}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <textarea id="bio" name="bio" />
+        </div>
+
+        <div>
+          <label htmlFor="job-location">Name</label>
+          <select id="job-location">
+            <option value="">Select a country</option>
+            <option value="US">United States</option>
+            <option value="GB">United Kingdom</option>
+            <option value="CA">Canada</option>
+            <option value="IN">India</option>
+            <option value="AU">Australia</option>
+          </select>
+        </div>
+
+        <div>
+          <label>
+            <input type="checkbox" id="terms" />I agree to the terms and
+            condition
+          </label>
+        </div>
+
+        <button>Submit</button>
+      </form>
+    </>
   );
 };
 
--- src/components/Application/Application.test.js
+++ src/components/Application/Application.test.js
@@ -4,8 +4,26 @@ import Application from "./Application";
 describe("Application", () => {
   test("Renders correctly", () => {
     render(<Application />);
-    const nameElement = screen.getByRole("textbox");
+
+    const pageHeading = screen.getByRole("heading", {
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole("heading", {
+      level: 2,
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
     expect(nameElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole("textbox", {
+      name: "Bio",
+    });
+    expect(bioElement).toBeInTheDocument();
 
     const jobLocationElement = screen.getByRole("combobox");
     expect(jobLocationElement).toBeInTheDocument();
@@ -15,5 +33,33 @@ describe("Application", () => {
 
     const submitButtonElement = screen.getByRole("button");
     expect(submitButtonElement).toBeInTheDocument();
+
+    const nameElement2 = screen.getByLabelText("Name", {
+      selector: "input",
+    });
+    expect(nameElement2).toBeInTheDocument();
+
+    const termsElement2 = screen.getByLabelText(
+      "I agree to the terms and condition"
+    );
+    expect(termsElement2).toBeInTheDocument();
+
+    const nameElement3 = screen.getByPlaceholderText("Fullname");
+    expect(nameElement3).toBeInTheDocument();
+
+    const paragraphElement = screen.getByText("ALl field are mandatory");
+    expect(paragraphElement).toBeInTheDocument();
+
+    const nameElement4 = screen.getByDisplayValue("Sandip Deb");
+    expect(nameElement4).toBeInTheDocument();
+
+    const imgElement = screen.getByAltText("a person with a laptop");
+    expect(imgElement).toBeInTheDocument();
+
+    const closeElement = screen.getByTitle("close");
+    expect(closeElement).toBeInTheDocument();
+
+    const customElement = screen.getByTestId("custom-element");
+    expect(customElement).toBeInTheDocument();
   });
 });
