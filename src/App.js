7eda2b7cb15363416ad9d33d54e47d27e398a608
SandipDeb <sandipdeb05@gmail.com>
2023-09-02 21:53:17 +0530

    learn http mock function

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import "./App.css";
 import Application from "./components/Application/Application";
 import Skills from "./components/Skills/Skills";
 import Counter from "./components/Counter/Counter";
-
 import AppProviders from "./components/Providers/AppProviders";
 import MuiMode from "./components/Mui/MuiMode";
+import Users from "./components/Users/Users";
 
 function App() {
   return (
     <AppProviders>
       <div className="App">
-        <MuiMode />
+        <Users />
       </div>
     </AppProviders>
   );
