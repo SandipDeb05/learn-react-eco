32ee43f92781f250f4f8f66d67bdc905d7dc8ddd
SandipDeb <sandipdeb05@gmail.com>
2023-09-02 09:44:40 +0530

    leaned user event testing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Application from "./components/Application/Application";
 import Skills from "./components/Skills/Skills";
+import Counter from "./components/Counter/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Application />
-      <Skills skills={["HTML", "CSS", "JS"]} />
+      {/* <Application /> */}
+      {/* <Skills skills={["HTML", "CSS", "JS"]} /> */}
+      <Counter />
     </div>
   );
 }
